ccdb3184ebcab4dbe680512b345a456018c03ef2
lordofbuckwheat <lord.of.buckwheat@gmail.com>
2018-12-03 07:08:57 +0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-pg';
+export class AppComponent implements OnInit {
+  public param1;
+  public param2;
+  constructor() {
+  }
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.param1 = 123;
+    }, 1000);
+    setTimeout(() => {
+      this.param2 = 456;
+    }, 1000);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule,ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from "@angular/common/http";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {AppComponent} from './app.component';
+import { DndComponent } from './dnd/dnd.component';
+import { TestComponent } from './test/test.component';
+import { DndStrictComponent } from './dnd-strict/dnd-strict.component';
+import { ComponentCreationComponent } from './component-creation/component-creation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DndComponent,
+    TestComponent,
+    DndStrictComponent,
+    ComponentCreationComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+  ],
+  bootstrap: [
+    AppComponent
+  ],
+  entryComponents: [
+    TestComponent
+  ]
 })
 export class AppModule { }
